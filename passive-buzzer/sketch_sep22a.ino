6699d0407130b08ad06c6759ca585ca722dc580f
Rodrigo Marin <rodrigofmarin@gmail.com>
2017-09-23 00:35:17 -0700

    initial passive buzzer experiment

---
+++ passive-buzzer/sketch_sep22a.ino
@@ -0,0 +1,10 @@
+int speakerPin = 11;
+int photocellPin = A0;
+void setup() {
+}
+
+void loop() {
+  int reading = analogRead(photocellPin);
+  int pitch = 200 + reading / 4;
+  tone(speakerPin, pitch);
+}
